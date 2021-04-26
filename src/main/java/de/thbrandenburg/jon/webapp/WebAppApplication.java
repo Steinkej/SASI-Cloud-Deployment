780a14f48039e84a488fb840d6b086fe112a4ac9
Jon <steinkej@th-brandenburg.de>
2021-04-26 11:44:22 +0200

    Second Commit mit Spring Boot Project

---
+++ src/main/java/de/thbrandenburg/jon/webapp/WebAppApplication.java
@@ -0,0 +1,26 @@
+package de.thbrandenburg.jon.webapp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
+
+
+
---
+++ src/test/java/de/thbrandenburg/jon/webapp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thbrandenburg.jon.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
